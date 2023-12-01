1ccd86e7883e3856beb86cd9541433288b9748ec
Huy278 <150950456+Huy278@users.noreply.github.com>
2023-12-01 10:12:09 -0500

    Awesome prototype
    The prototype code

--- AdventureGameBased/src/Player.java
+++ AdventureGameBased/src/Player.java
@@ -1,13 +1,29 @@
 
 public class Player {
-	private int health, soul;
-	private double defence;
+	private String name;
+	private int soul;
+	private double health, defence;
 	private Weapon weapon1, weapon2, weapon3;
-	public Player() {
+	public Player(String name) {
+		this.name=name;
 		health = 100;
 		soul = 0;
 		defence=1;
-		
+		weapon1 = new Weapon("Pitbull", 5.0);
 	}
-
+	public Weapon getPlayerWeapon1(){
+		return weapon1;
+	}
+	public double getHealth(){
+		return health;
+	}
+	public String getName(){
+		return name;
+	}
+	public void takingDamage(double damageIn){
+		health-=(damageIn/defence);
+	}
+	public String toString(){
+		return "Player: " + name + " | "+ health + " Health | " + weapon1;
+	}
 }
