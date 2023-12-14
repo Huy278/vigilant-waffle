8d8fd30fe890ef53a1e6a0aea3c30f017dae7cde
Fortnighmaster <lcolon2025@jpkeefehs.org>
2023-12-14 09:21:05 -0500

    j
    yu

--- AdventureGameBased/src/Enemy.java
+++ AdventureGameBased/src/Enemy.java
@@ -3,10 +3,16 @@ public class Enemy {
 	MySQLConnect mySQL = new MySQLConnect("adventure");
 	private String name;
 	private double health, dmgMult;
-	private int soul, defence, tier;
+	private int souls, defence, tier;
 	private Weapon eWeapon;
-	public Enemy(String name) {
-		
+	public Enemy(String name, double health, double dmgMult, int souls, int tier, Weapon eWeapon) {
+		this.name=name;
+		this.health=health;
+		this.dmgMult=dmgMult;
+		this.souls=souls;
+		this.tier=tier;
+		this.eWeapon=eWeapon;
+		defence=1;
 	}
 	public String toString(){
 		return "Enemy: " + name + " | "+ health + " Health | " + eWeapon;
