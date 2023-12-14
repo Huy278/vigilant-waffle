dff7aea2bf5780c4ff6427868fe03c60e3396e2d
ca800dev <144160431+ca800dev@users.noreply.github.com>
2023-12-14 10:07:25 -0500

    mysql stuff
    takes mysql database and shoves it into an object array to be used for enemy instantiation (note: may require modification.)

--- AdventureGameBased/src/MySQLConnect.java
+++ AdventureGameBased/src/MySQLConnect.java
@@ -9,20 +9,24 @@ public class MySQLConnect {
 	Connection connection = null;
 	Statement statement = null;
     public MySQLConnect(String db) {
-        String url = "jdbc:mysql://localhost:3306/" + db;
+    	
+        String url = "jdbc:mysql://localhost:3306/"+db;
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("0");
+        	Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, "root", "Keefe2012");
             statement = connection.createStatement();
-        }
-        catch (Exception e) {
+
+       }
+       catch (Exception e) {
             System.out.println(e);
         }    
     }
    
     public ResultSet select(String sql) {
         ResultSet resultSet = null;
+        
         try {
             resultSet = statement.executeQuery(sql);
         } catch (SQLException e) {
@@ -30,5 +34,37 @@ public class MySQLConnect {
             e.printStackTrace();
         }
         return resultSet;
+    }
+    
+    public Object[][] unpackEnemyDatabase(){
+    	ResultSet results;
+    	int height = 0;
+    	try {
+    	results=statement.executeQuery("select count(id) from enemy;");
+    	
+		
+			height = 7;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	Object[][] arr = new Object[height][5];
+    	int i = 0;
+    	try {
+    		results=statement.executeQuery("select * from enemy;");
+			while (results.next()) {
+					arr[i][0] = results.getString(2);
+					arr[i][1] = results.getInt(3);
+					arr[i][2] = results.getDouble(4);
+					arr[i][3] = results.getInt(5);
+					arr[i][4] = results.getInt(6);
+					i++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return arr;
     }
 }
